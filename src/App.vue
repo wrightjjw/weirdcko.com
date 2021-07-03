1b55bcf9630d271cd0741728d6b530204ec0573d
Josh Wright <wright.jjw@gmail.com>
2021-07-02 21:28:36 -0400

    created style.css

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,13 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 #nav {
   padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #607a8a;
 }
 
 #nav a.router-link-exact-active {
